158e9b8e7dcc6564c99c7af3c231b0fb8fa63e7f
Hieu To <tothieu160304@gmail.com>
2025-11-05 15:27:08 +0700

    ..

--- apps/orders/migrations/0005_add_order_name_field.py
+++ apps/orders/migrations/0005_add_order_name_field.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('orders', '0001_initial'),
+        ('orders', '0004_orderitem_note'),
     ]
 
     operations = [
